93f0dcf580b75f3668eab02573536ec975466bf3
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-12-26 00:10:58 +0100

    updates

--- src/settings.js
+++ src/settings.js
@@ -2,6 +2,6 @@
 Defaults for site
 * */
 export const app = {
-    BASE_URL: 'http://musicmaxx.herokuapp.com/',
+    BASE_URL: 'https://musicmaxx.herokuapp.com/',
     LOCAL_BASE_URL: 'http://127.0.0.1:8000/',
 };
